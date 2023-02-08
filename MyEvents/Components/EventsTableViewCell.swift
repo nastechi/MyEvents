d19ce98649cbfe81e3575e0bd8b37a43e7a90743
Anastasia Boriskina <a.boriskina@outlook.com>
2023-02-08 11:36:50 +0500

    Update tableViewCell by adding imageView.

--- MyEvents/Components/EventsTableViewCell.swift
+++ MyEvents/Components/EventsTableViewCell.swift
@@ -24,6 +24,8 @@ class EventsTableViewCell: UITableViewCell {
     
     private lazy var eventImageView: UIImageView = {
         let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "cloud")
+        imageView.contentMode = .scaleAspectFit
         return imageView
     }()
     
@@ -45,12 +47,14 @@ class EventsTableViewCell: UITableViewCell {
         if let date = event.date {
             dateLabel.text = getDateString(date: date)
         }
+        setImage(with: event.imageUrl)
         layoutView()
     }
     
     func layoutView() {
         addSubview(dateLabel)
         addSubview(nameLabel)
+        addSubview(eventImageView)
         setConstrains()
     }
     
@@ -64,12 +68,32 @@ class EventsTableViewCell: UITableViewCell {
         nameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
         nameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
         nameLabel.topAnchor.constraint(equalTo: dateLabel.bottomAnchor, constant: 6).isActive = true
-        nameLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16).isActive = true
+        
+        eventImageView.translatesAutoresizingMaskIntoConstraints = false
+        eventImageView.heightAnchor.constraint(equalToConstant: 250).isActive = true
+        eventImageView.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 6).isActive = true
+        eventImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
+        eventImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16).isActive = true
+        eventImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16).isActive = true
     }
     
     func getDateString(date: Date) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "EEEE, d MMM, HH:mm"
         return dateFormatter.string(from: date)
+    }
+    
+    func setImage(with urlString: String?) {
+        guard urlString != nil else { return }
+        guard let url = URL(string: urlString!) else { return }
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.eventImageView.image = image
+                    }
+                }
+            }
+        }
     }
 }
